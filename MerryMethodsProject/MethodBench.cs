caeee93d65cef460564939d3c72e35f64a207054
Aynamik4 <violintailgut@gmail.com>
2021-01-18 17:30:44 +0100

    Initial commit

---
+++ MerryMethodsProject/MethodBench.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MerryMethodsProject
+{
+    class MethodBench
+    {
+        public void FirstMethod()
+        {
+            Console.WriteLine("Merry Methods");
+        }
+
+        public void SecondMethod(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine();
+        }
+
+        public void ThirdMethod(string message, bool versaler)
+        {
+            if (versaler)
+                message = message.ToUpper();
+            else
+                message = message.ToLower();
+
+            SecondMethod(message);
+        }
+
+        public bool FourthMethod()
+        {
+            bool validKey = false;
+            ConsoleKey key = ConsoleKey.A;
+
+            while (!validKey)
+            {
+                Console.Write("Would you like to [W]hisper or [S]hout?: ");
+                key = Console.ReadKey(false).Key;
+                validKey = (key == ConsoleKey.W) || (key == ConsoleKey.S);
+            }
+
+            Console.WriteLine();
+            return key == ConsoleKey.S;
+        }
+
+        public (string Name, bool Shout) WhosSounding()
+        {
+            Console.Write("Please type your name: ");
+            return (Console.ReadLine(), FourthMethod());
+        }
+
+        public (string Name, bool LengthyName) SixthMethod()
+        {
+            Console.Write("Please type a name (any name): ");
+            string s = Console.ReadLine();
+            bool longName = s.Length > 5;
+            return (s, longName);
+        }
+    }
+}
---
+++ MerryMethodsProject/Program.cs
@@ -0,0 +1,43 @@
+﻿// Ugh!
+
+using System;
+using System.Collections.Generic;
+
+namespace MerryMethodsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodBench methodBench = new MethodBench();
+
+            int count = 0;
+            do
+            {
+                count++;
+                methodBench.FirstMethod();
+            } while (count < 3);
+
+            methodBench.SecondMethod("C# Now and Forever");
+            Console.Write("Enter something: ");
+            string input = Console.ReadLine();
+            methodBench.SecondMethod(input);
+
+            Console.Write("[U] to print in upper case, otherwise lower case: ");
+            bool upper = Console.ReadKey(false).Key == ConsoleKey.U;
+            Console.WriteLine();
+            methodBench.ThirdMethod(input, upper);
+
+            bool shout = methodBench.FourthMethod();
+            methodBench.ThirdMethod(input, shout);
+            methodBench.ThirdMethod(input, methodBench.FourthMethod());
+
+            var x = methodBench.SixthMethod();
+
+            if(x.LengthyName)
+                Console.WriteLine($"\"{x.Name}\" is a lengthy name.");
+            else
+                Console.WriteLine($"\"{x.Name}\" is not a lengthy name.");
+        }
+    }
+}
